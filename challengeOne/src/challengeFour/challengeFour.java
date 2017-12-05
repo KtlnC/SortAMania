1293053e3f7ff32475ded348805c87bfd32ad682
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-12-05 12:31:44 -0500

    Started a challenge >:)

--- challengeOne/src/challengeFour/challengeFour.java
+++ challengeOne/src/challengeFour/challengeFour.java
@@ -1,5 +1,17 @@
 package challengeFour;
-
+import challengeOne.challengeOne;;
 public class challengeFour {
 
+	 public static void main(String[]args)
+	 {
+		 
+	 }
+	 public static int[] sortarr(int[][]arr)
+	 {
+		 int[] list = new int[2];
+		 for(int i = 0;i<arr.length;i++)
+		 {
+			 challengeOne.median(arr[i]);
+		 }
+	 }
 }
